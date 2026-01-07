86989c9220cbcaf781d4d76f404bc35b73c09d46
Jean Dioubaté <jean.dioubate@fms-ea.com>
2026-01-07 15:07:47 +0100

    implémentation de la couche application (main), déclaration de la classe Main, création de la variable BusinessTraining et du Scanner pour lire la saisie utilisateur.

--- src/fr/ldnr/application/Main.java
+++ src/fr/ldnr/application/Main.java
@@ -1,52 +1,19 @@
 package fr.ldnr.application;
 
 import fr.ldnr.entity.Training;
-import fr.ldnr.dao.TrainingDao;
+import fr.ldnr.business.BusinessTraining;
 import fr.ldnr.dao.TrainingDaoImpl;
 
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
-		/*
-        // Création de quelques formations
-        Training t1 = new Training(1, "Java Basics", "Introduction to Java", 5, "PRESENTIEL", 500.0);
-        Training t2 = new Training(2, "Python Advanced", "Advanced Python topics", 3, "DISTANCIEL", 300.0);
-        Training t3 = new Training(3, "Data Analysis", "Learn to analyze data", 4, "PRESENTIEL", 400.0);
-
-        // Affichage des formations
-        System.out.println(t1);
-        System.out.println(t2);
-        System.out.println(t3);
-
-        // Accès aux getters
-        System.out.println("Durée de la formation " + t1.getName() + " : " + t1.getDurationInDays() + " jours");
-        System.out.println("Prix de la formation " + t2.getName() + " : " + t2.getPrice() + " €");
-*/	
-		TrainingDao dao = new TrainingDaoImpl();
-
-        // ===== Test readAll() =====
-        System.out.println("=== TEST readAll() ===");
-        List<Training> allTrainings = dao.readAll();
-        for (Training t : allTrainings) {
-            System.out.println(t);
-        }
-
-        // ===== Test readByKeyword() =====
-        System.out.println("\n=== TEST readByKeyword(\"Java\") ===");
-        List<Training> javaTrainings = dao.readByKeyword("Java");
-        for (Training t : javaTrainings) {
-            System.out.println(t);
-        }
-
-        // ===== Test readByModality() =====
-        System.out.println("\n=== TEST readByModality(\"PRESENTIEL\") ===");
-        List<Training> presentielTrainings = dao.readByModality("PRESENTIEL");
-        for (Training t : presentielTrainings) {
-            System.out.println(t);
-        }	
+		BusinessTraining business = new BusinessTraining(new TrainingDaoImpl());
+		Scanner scan = new Scanner(System.in);
+			
 	}
 
 }
