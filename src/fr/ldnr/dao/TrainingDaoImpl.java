ccf7a8e853277295a4d026004777cb2214713722
Jean Dioubaté <jean.dioubate@fms-ea.com>
2026-01-07 08:19:47 +0100

    Correction et implémentation de la class TrainingDaoImpl utilisant les méthodes readAll, readByKeyword et readByModality qui vont récupérer les données depuis la bdd grâce aux requêtes sql.

--- src/fr/ldnr/dao/TrainingDaoImpl.java
+++ src/fr/ldnr/dao/TrainingDaoImpl.java
@@ -1,10 +1,105 @@
 package fr.ldnr.dao;
 
-public class TrainingDaoImpl {
+import fr.ldnr.entity.Training;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
+public class TrainingDaoImpl implements TrainingDao {
+	
+	private static final String URL = "jdbc:mariaDB://localhost:3308/Sales";
+	private static final String USER = "root";
+	private static final String PASSWORD = "fms2025";
+	
+	@Override
+	public List<Training> readAll() {
+		List<Training> courses =  new ArrayList<>();
+		String sql = "SELECT * FROM training";
+		
+		try (Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
+			 Statement statement = connection.createStatement();
+			 ResultSet rs = statement.executeQuery(sql)) {
+			
+			while (rs.next()) {
+				Training training = new Training(
+						rs.getInt("t_id"),
+						rs.getString("t_name"),
+						rs.getString("description"),
+						rs.getInt("t_time"),
+						rs.getString("t_modality"),
+						rs.getDouble("t_price")
+			  );
+			  courses.add(training);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace(); // 
+		}
+		
+		return courses;
 	}
+	
+	
+	@Override
+	public List<Training> readByKeyword(String keyword) {
+	    List<Training> courses = new ArrayList<>();
+	    String sql = "SELECT * FROM training WHERE t_name LIKE ?";
+
+	    try (Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
+	         PreparedStatement ps = connection.prepareStatement(sql)) {
+
+	        ps.setString(1, "%" + keyword + "%");
+	        ResultSet rs = ps.executeQuery();
+
+	        while (rs.next()) {
+	            Training training = new Training(
+	                    rs.getInt("t_id"),
+	                    rs.getString("t_name"),
+	                    rs.getString("description"),
+	                    rs.getInt("t_time"),
+	                    rs.getString("t_modality"),
+	                    rs.getDouble("t_price")
+	            );
+	            courses.add(training);
+	        }
+
+	    } catch (SQLException e) {
+	        e.printStackTrace();
+	    }
+
+	    return courses;
+	}
+	
+	
+	@Override
+	public List<Training> readByModality(String modality) {
+	    List<Training> courses = new ArrayList<>();
+	    String sql = "SELECT * FROM training WHERE t_modality = ?";
+
+	    try (Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
+	         PreparedStatement ps = connection.prepareStatement(sql)) {
+
+	        ps.setString(1, modality);
+	        ResultSet rs = ps.executeQuery();
+
+	        while (rs.next()) {
+	            Training training = new Training(
+	                    rs.getInt("t_id"),
+	                    rs.getString("t_name"),
+	                    rs.getString("description"),
+	                    rs.getInt("t_time"),
+	                    rs.getString("t_modality"),
+	                    rs.getDouble("t_price")
+	            );
+	            courses.add(training);
+	        }
+
+	    } catch (SQLException e) {
+	        e.printStackTrace();
+	    }
+
+	    return courses;
+	}
+
 
 }
