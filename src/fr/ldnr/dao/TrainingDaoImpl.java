a1f3cb0fb73478ba12a7ec588754bf0e3e702e77
Jean Dioubaté <jean.dioubate@fms-ea.com>
2026-01-08 14:47:46 +0100

    ajout de quelques commentaires dans la couche dao implémentation.

--- src/fr/ldnr/dao/TrainingDaoImpl.java
+++ src/fr/ldnr/dao/TrainingDaoImpl.java
@@ -6,12 +6,21 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Implémentation de l'interface (TrainingDao).
+ * Cette classe permet l'accès aux données des formations sotckées
+ * dans une base de données MariaDB via JDBC.
+ */
 public class TrainingDaoImpl implements TrainingDao {
 	
 	private static final String URL = "jdbc:mariadb://localhost:3308/Sales";
 	private static final String USER = "root";
 	private static final String PASSWORD = "fms2025";
 	
+	/**
+	 * Récupère toutes les formations présentes dans la base de données.
+	 * @return une liste contenant toutes les formations.
+	 */
 	@Override
 	public List<Training> readAll() {
 		List<Training> courses =  new ArrayList<>();
@@ -39,7 +48,11 @@ public class TrainingDaoImpl implements TrainingDao {
 		return courses;
 	}
 	
-	
+	/**
+	 * Récupère les formations dont le nom contient un mot-clé donné.
+	 * @param keyword le mot-clé utilisé pour la recherche
+	 * @return une liste de formations correspondant au mot-clé.
+	 */
 	@Override
 	public List<Training> readByKeyword(String keyword) {
 	    List<Training> courses = new ArrayList<>();
@@ -70,7 +83,11 @@ public class TrainingDaoImpl implements TrainingDao {
 	    return courses;
 	}
 	
-	
+	/**
+	 * Récupère les formations selon leur modalité.
+	 * @param modality la modalité de la formation (PRESENTIEL ou DISTANCIEL).
+	 * @return une liste de formations correspondant à la modalité donnée.
+	 */
 	@Override
 	public List<Training> readByModality(String modality) {
 	    List<Training> courses = new ArrayList<>();
